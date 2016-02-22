57a1c736ab7e110ff86c9651196f4617eddca413
Tbouder <tbouder@protonmail.com>
2016-02-22 21:15:54 +0100

    Auto-Update 21:15:54, Lundi 22 février

--- ft_link_one.c
+++ ft_link_one.c
@@ -6,7 +6,7 @@
 /*   By: Tbouder <Tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/21 20:01:41 by Tbouder           #+#    #+#             */
-/*   Updated: 2016/02/22 18:32:24 by Tbouder          ###   ########.fr       */
+/*   Updated: 2016/02/22 21:14:30 by Tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void		ft_link_down_to_up_h(t_win w, t_dot *dot, t_dot *dot_next) //GREEN
 	coef_y = (float)w.zoom / (-(dot_next->y - dot->y));
 	x = dot->x;
 	y = dot->y;
-	while (y > dot_next->y)
+	while (y > dot_next->y && x <= w.max_x - 100 && y <= w.max_y - 100)
 	{
 		mlx_pixel_put(w.mlx, w.window, x, y, 65408);
 		x += coef_y;
@@ -38,7 +38,7 @@ static void		ft_link_up_to_down_h(t_win w, t_dot *dot, t_dot *dot_next) //BLUE
 	coef_y = (float)w.zoom / (dot_next->y - dot->y);
 	x = dot->x;
 	y = dot->y;
-	while (y < dot_next->y)
+	while (y < dot_next->y && x <= w.max_x - 100 && y <= w.max_y - 100)
 	{
 		mlx_pixel_put(w.mlx, w.window, x, y, 3113674);
 		x += coef_y;
