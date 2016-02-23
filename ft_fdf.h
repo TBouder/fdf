3cad054f5d5aeb4b9d2870bd853a34d21d9a54d3
Tbouder <tbouder@student.42.fr>
2016-02-23 12:54:40 +0100

    Auto-Update 12:54:40, Mardi 23 février

--- ft_fdf.h
+++ ft_fdf.h
@@ -6,7 +6,7 @@
 /*   By: Tbouder <Tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 14:23:56 by Tbouder           #+#    #+#             */
-/*   Updated: 2016/02/22 21:37:32 by Tbouder          ###   ########.fr       */
+/*   Updated: 2016/02/23 12:32:29 by Tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 
 # include <stdio.h>//
 
-# define ZOOM 5
+# define ZOOM 15
 # define POS_X 0
 # define POS_Y (w.max_y / 2)
 
