b131d095bdf0078816c2cd909415fee8d14d451c
Tbouder <tbouder@student.42.fr>
2016-02-25 13:01:13 +0100

    Auto-Update 13:01:13, Jeudi 25 février

--- ft_link_two.c
+++ ft_link_two.c
@@ -6,7 +6,7 @@
 /*   By: Tbouder <Tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 19:23:22 by Tbouder           #+#    #+#             */
-/*   Updated: 2016/02/25 12:29:57 by Tbouder          ###   ########.fr       */
+/*   Updated: 2016/02/25 12:43:41 by Tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void		ft_link_down_to_up_v(t_win w, t_dot *dot, t_dot *dot_next) //Red
 	coef_y = (float)w.zoom / (-(dot_next->y - dot->y));
 	x = dot->x;
 	y = dot->y;
-
+	printf("%f vs %d\n", y, dot_next->y);
 	while (coef_y > 0 && y > dot_next->y)
 	{
 		mlx_pixel_put(w.mlx, w.window, x, y, 16669806);
@@ -75,15 +75,15 @@ void			ft_link_two(t_win w, t_dot *dot)
 		dot = dot->next;
 		dot_next = ft_dotnext(dot, w.x);
 	}
-
-	dot_next = dot_save->next;
-	while (dot_next)
-	{
-		if (dot_next->z > dot_save->z)
-			ft_link_up_to_down_v(w, dot_save, dot_next);
-		if (dot_next->z < dot_save->z && dot_save->id % w.x != w.x - 1)
-			ft_link_down_to_up_v(w, dot_save, dot_next);
-		dot_save = dot_save->next;
-		dot_next = dot_save->next;
-	}
+	printf("\n\n");
+	// dot_next = dot_save->next;
+	// while (dot_next)
+	// {
+	// 	if (dot_next->z > dot_save->z)
+	// 		ft_link_up_to_down_v(w, dot_save, dot_next);
+	// 	if (dot_next->z < dot_save->z && dot_save->id % w.x != w.x - 1)
+	// 		ft_link_down_to_up_v(w, dot_save, dot_next);
+	// 	dot_save = dot_save->next;
+	// 	dot_next = dot_save->next;
+	// }
 }
